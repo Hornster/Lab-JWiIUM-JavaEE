37f09e92e9630b6c72c48b351d2ff899cab71bc1
Karol Kozuch <karol199714@gmail.com>
2019-01-03 22:41:15 +0100

    Prepared servlets for the new database. Now, tests shall begin.

--- src/java/pl/polsl/controller/server/servlets/HistoryRetrieverServlet.java
+++ src/java/pl/polsl/controller/server/servlets/HistoryRetrieverServlet.java
@@ -13,7 +13,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import pl.polsl.model.BackendContainer;
 import pl.polsl.model.queryHistory.SingleQuery;
 import pl.polsl.model.querydb.DBManagerIniException;
 import pl.polsl.model.querydb.QueryDBManager;
